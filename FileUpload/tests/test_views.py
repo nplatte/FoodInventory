0911a3ecb17b51ac39a1f9244403faff5a0ef4c5
Nathan Platte <nplatte0@gmail.com>
2024-12-06 17:56:00 -0500

    adjusted unittests to use SimpleUploadedFile

--- FileUpload/tests/test_views.py
+++ FileUpload/tests/test_views.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.core.files.uploadedfile import SimpleUploadedFile
 
 class TestFileUpload(TestCase):
 
@@ -13,14 +14,14 @@ class TestFileUpload(TestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_good_POST_returns_201(self):
-        f = open("FileUpload/tests/invoice.pdf", "r")
+        f = SimpleUploadedFile("FileUpload/tests/invoice.pdf", b'Simple content', content_type='application/pdf')
         response = self.client.post(self.url, {"file": f})
         self.assertEqual(response.status_code, 201)
         self.assertEqual(len(response.context), 1)
         self.assertIn(response.context["recent"], "invoice.pdf")
 
     def test_bad_POST_returns_401(self):
-        f = open("FileUpload/tests/bad.txt", "r")
+        f = SimpleUploadedFile("FileUpload/tests/invoice.txt", b'Simple content', content_type='text/plain')
         response = self.client.post(self.url, {"file": f})
         self.assertEqual(response.status_code, 401)
         self.assertEqual(len(response.context), 0)
