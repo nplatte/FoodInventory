93e80718fa52a50610638d4666039b99130840fd
Nathan Platte <nplatte0@gmail.com>
2024-12-05 20:08:44 -0500

    added functional test and files

--- functional_tests/test.py
+++ functional_tests/test.py
@@ -1,4 +1,6 @@
 from django.test import TestCase
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 class TestFileUpload(TestCase):
 
@@ -9,7 +11,20 @@ class TestFileUpload(TestCase):
         return super().tearDown()
     
     def test_user_can_upload_pdf_file(self):
-        pass
+        self.browser = webdriver.Chrome()
+        self.url = "localhost:8000"
+        # go to the webpage
+        self.browser.get(self.url)
+        self.assertEqual("File Upload", self.browser.title)
+        # find the file upload
+        pdf_upload = self.browser.find_element(By.ID, "pdf_upload")
+        submit = self.browser.find_element(By.ID, "submit")
+        # upload the file
+        pdf_upload.send_keys("functional_tests/invoice.pdf")
+        submit.click()
+        # verify that the file pops up in the window
+        files = self.browser.find_elements(By.CLASS_NAME, "file")
+        self.assertEqual(1, len(files))        
 
     def test_user_cannot_upload_non_pdf_file(self):
         pass
