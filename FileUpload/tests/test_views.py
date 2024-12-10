1355708b35fb7764af104c6f2dbdb48554176bd3
Nathan Platte <nplatte0@gmail.com>
2024-12-10 08:42:55 -0500

    view takes file, checks if its a pdf, and adds it to the context

--- FileUpload/tests/test_views.py
+++ FileUpload/tests/test_views.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.core.files.uploadedfile import SimpleUploadedFile
+from FileUpload.forms import FileForm
 
 class TestFileUpload(TestCase):
 
@@ -15,13 +16,20 @@ class TestFileUpload(TestCase):
 
     def test_good_POST_returns_201(self):
         f = SimpleUploadedFile("FileUpload/tests/invoice.pdf", b'Simple content', content_type='application/pdf')
-        response = self.client.post(self.url, {"file": f})
-        self.assertEqual(response.status_code, 201)
-        self.assertEqual(len(response.context), 1)
-        self.assertIn(response.context["recent"], "invoice.pdf")
+        response = self.client.post(self.url, {"new_file": f})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.context['recent']), 1)
+        self.assertIn('invoice.pdf', response.context["recent"])
 
-    def test_bad_POST_returns_401(self):
+    def test_bad_POST_returns_400(self):
         f = SimpleUploadedFile("FileUpload/tests/invoice.txt", b'Simple content', content_type='text/plain')
-        response = self.client.post(self.url, {"file": f})
-        self.assertEqual(response.status_code, 401)
-        self.assertEqual(len(response.context), 0)
+        response = self.client.post(self.url, {"new_file": f})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.context['recent']), 0)
+
+    def test_correct_form_used(self):
+        get = self.client.get(self.url)
+        f = SimpleUploadedFile("FileUpload/tests/invoice.pdf", b'Simple content', content_type='application/pdf')
+        post = self.client.post(self.url, {"new_file": f})
+        self.assertIsInstance(get.context['form'], FileForm)
+        self.assertIsInstance(post.context['form'], FileForm)
--- FileUpload/views.py
+++ FileUpload/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from .forms import FileForm
+from django.http.response import HttpResponseBadRequest
 
 def AddFileView(request):
-    file_upload_form = FileForm()
+    form = FileForm()
+    context = {}
+    context['form'] = form
+    context['recent'] = []
     if request.method == "POST":
-        pass
-    return render(request, "file_upload.html")
+        filled_upload = FileForm(request.POST, request.FILES)
+        context['form'] = filled_upload
+        if filled_upload.is_valid():
+            context['recent'].append(request.FILES['new_file'].name)
+    return render(request, "file_upload.html", context)
